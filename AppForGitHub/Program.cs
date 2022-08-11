ba1e01b15db69826b2453e77e4f54337b3bbc348
bhaveshGH <chaudharibhavesh0709@gmail.com>
2022-08-11 09:46:23 +0530

    Add project files.

---
+++ AppForGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppForGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
